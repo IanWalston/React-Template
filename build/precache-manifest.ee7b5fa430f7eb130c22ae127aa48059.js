9f385301cb9688357aee0beb9cda65fb2a8a61b8
Ian Walston <ikwalston@comcast.net>
2019-03-12 22:09:01 -0400

    build

---
+++ build/precache-manifest.ee7b5fa430f7eb130c22ae127aa48059.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "1ce99f0ef88439be0996",
+    "url": "./static/css/main.c20b6dc8.chunk.css"
+  },
+  {
+    "revision": "1ce99f0ef88439be0996",
+    "url": "./static/js/main.1ce99f0e.chunk.js"
+  },
+  {
+    "revision": "dec0e7115ebafbcbd526",
+    "url": "./static/js/1.dec0e711.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "12b8d9af89da22b5c53dcffbca412808",
+    "url": "./static/media/img_parallax2.12b8d9af.jpg"
+  },
+  {
+    "revision": "91f966b59c584174ed00c5a9963ecba3",
+    "url": "./static/media/img_parallax.91f966b5.jpg"
+  },
+  {
+    "revision": "73129a4352952fdc00fcc7e55ef7a52f",
+    "url": "./static/media/img_parallax3.73129a43.jpg"
+  },
+  {
+    "revision": "7f869cb0cad1464bb7567896bdc42c43",
+    "url": "./index.html"
+  }
+];
