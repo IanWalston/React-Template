18584d9d1d1aa428d6635aea31214a2b4bcb2ab0
Ian Walston <ikwalston@comcast.net>
2019-03-12 22:30:20 -0400

    build

---
+++ build/precache-manifest.3eb65737f23971569a39c7c01b5e2e25.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "dac4193e00e23b439ded",
+    "url": "./static/css/main.f044f8e6.chunk.css"
+  },
+  {
+    "revision": "dac4193e00e23b439ded",
+    "url": "./static/js/main.dac4193e.chunk.js"
+  },
+  {
+    "revision": "dec0e7115ebafbcbd526",
+    "url": "./static/js/1.dec0e711.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "12b8d9af89da22b5c53dcffbca412808",
+    "url": "./static/media/img_parallax2.12b8d9af.jpg"
+  },
+  {
+    "revision": "91f966b59c584174ed00c5a9963ecba3",
+    "url": "./static/media/img_parallax.91f966b5.jpg"
+  },
+  {
+    "revision": "73129a4352952fdc00fcc7e55ef7a52f",
+    "url": "./static/media/img_parallax3.73129a43.jpg"
+  },
+  {
+    "revision": "12d341e5c64eb4cf5e954f3b378ddfcd",
+    "url": "./index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.ee7b5fa430f7eb130c22ae127aa48059.js"
+  "./precache-manifest.3eb65737f23971569a39c7c01b5e2e25.js"
 );
 
 workbox.clientsClaim();
